4f0d5526559d67644daebb8e854c0bec6a843dd5
Sam Pan <pwl12344321@gmail.com>
2022-07-14 00:13:06 +0800

    count

--- random1.py
+++ random1.py
@@ -6,13 +6,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	x = input('please guess the number between 1-100: ')
 	x = int(x)
 	if x == r:
 		print('You are correct')
+		print ('This is the', count, 'time(s)')
 		break
 	elif x > r:
 		print('lower')
 	elif x < r:
 		print('higher')
+	print ('This is the', count, 'time(s)')
