1d635ce0f7acba4a8c1e2566d587c04c954924a6
Sam Pan <pwl12344321@gmail.com>
2022-07-14 00:19:12 +0800

    final version

--- random1.py
+++ random1.py
@@ -1,15 +1,19 @@
-# 產生一個隨機整數 1~10 (不要印出來)
+# 產生一個隨機整數 1~100 (不要印出來)
 # 使用者可重複猜
 # 猜對 印出"You are correct"
 # 猜錯 提示他 比答案大/小
 
 import random
 
-r = random.randint(1, 100)
+a = input('start in: ')
+b = input('End in: ')
+a = int(a)
+b = int(b)
+r = random.randint(a, b)
 count = 0
 while True:
 	count += 1 # count = count + 1
-	x = input('please guess the number between 1-100: ')
+	x = input('please guess the number: ')
 	x = int(x)
 	if x == r:
 		print('You are correct')
